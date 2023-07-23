44a7563780b9a54b5e47d511325df1b44b05f9b1
gaolingjie1205 <70207756+gaolingjie1205@users.noreply.github.com>
2023-07-23 11:34:18 +0800

    导入了字体图标、引入了vant组件库

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import store from './store'
 // 引入全局scss样式
 import '@/styles/index.scss'
 
+// 引入vant组件库
+import { Button } from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
